3e83c7da0b55f335b62faca51a6810174c61337a
ThuySeven23 <huuthuytrann3004@gmail.com>
2025-02-14 23:09:39 +0700

    upload: coding ui homepage successfully

---
+++ frontend/my-project/src/interfaces/interfaces.ts
@@ -0,0 +1,27 @@
+export interface Category {
+    title: string,
+    image: string, 
+}
+
+export interface Banner {
+    title: string,
+    subtitle: string,
+    discount: string,
+    buttonText: string,
+    image: string, 
+    bgColorFrom: string,
+    bgColorTo: string,
+}
+
+export interface BigZone {
+    title: string,
+    subtitle: string,
+    discount: string,
+    image: string,
+    bgColor: string,
+}
+
+export interface CategoriesBoth {
+    name: string,
+    image: string,
+}
